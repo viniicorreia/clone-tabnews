dd3e60f68b8182eb8328dffe9ff733b3a7aa66b3
Vinicius Correia <viniciuscorreia@MacBook-Pro-de-Vinicius.local>
2024-08-10 05:39:35 -0300

    adding next and react

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c31127d4bd6d1173"; }
+/******/ 		__webpack_require__.h = function() { return "f7085a1a37118938"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.5eabc0fab7390708.hot-update.js
+++ .next/static/webpack/webpack.5eabc0fab7390708.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c31127d4bd6d1173"; }
+/******/ 	__webpack_require__.h = function() { return "f7085a1a37118938"; }
 /******/ }();
 /******/ 
 /******/ }
