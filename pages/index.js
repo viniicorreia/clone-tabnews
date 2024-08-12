015e53e4e65b9afceeb04091d9478f9cfd35bd90
Vinícius Correia <softmakerinfos@gmail.com>
2024-08-12 07:34:33 -0300

    amend

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 function Home (){
-    return <h1>teste vini force</h1>
-    return <h1><strong>Jujuba</strong>, eu amo você. Se você me ama da uma risadinha!</h1>
+    return <h1><strong>Jujuba</strong>, eu amo você. Se você me ama da uma risadinha!! </h1>
 }
-
 export default Home
