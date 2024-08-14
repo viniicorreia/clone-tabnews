d1092240dce43c0f496417a39d625feddcc21e91
Vinícius Correia <softmakerinfos@gmail.com>
2024-08-14 07:41:00 -0300

    mensagem para o mozao

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home (){
-    return <h1><strong>Jujuba</strong>, eu amo você. Se você me ama da uma risadinha!!!! 😎 💛</h1>
+    return <h1> Oi <strong>mo</strong>, eu amo você. 💛💛</h1>
 }
 export default Home
