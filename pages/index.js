22513115519de58d9f2d842767a4a71bf9a04b38
Vinícius Correia <softmakerinfos@gmail.com>
2024-08-14 07:20:59 -0300

    adding emojis

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home (){
-    return <h1><strong>Jujuba</strong>, eu amo você. Se você me ama da uma risadinha!! </h1>
+    return <h1><strong>Jujuba</strong>, eu amo você. Se você me ama da uma risadinha!!!! 😎 💛</h1>
 }
 export default Home
