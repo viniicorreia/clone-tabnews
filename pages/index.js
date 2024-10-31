545dba1e3845f35fe6f6d081868b1811e67db2dd
Vinícius Correia <vinicius@buzzlead.com.br>
2024-10-30 21:19:51 -0300

    #fast fix mo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home (){
-    return <h1> Oi <strong>mo</strong>, eu amo você. 💛💛</h1>
+function Home() {
+    return <h1> Oi <strong>mô</strong>, eu amo você. 💛💛</h1>
 }
 export default Home
